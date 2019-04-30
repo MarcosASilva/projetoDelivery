98dbe4aea13906766c906247e47ad57a3c29e429
Marcos Assis <marcosvlk15@gmail.com>
2019-04-30 16:51:33 -0300

    Novas páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'restaurante', loadChildren: './restaurante/restaurante.module#RestaurantePageModule' }
 ];
 
 @NgModule({
