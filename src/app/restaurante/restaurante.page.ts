033431df3672f940b90efb4b219e8f3b6d016509
Unknown <Marcosvlk15@gmail.com>
2019-05-03 20:00:49 -0300

    mudanças realizada

--- src/app/restaurante/restaurante.page.ts
+++ src/app/restaurante/restaurante.page.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurante.page.scss'],
 })
 export class RestaurantePage implements OnInit {
-
-  constructor() { }
+  qtd = 0;
+  cardapio = [ ];
+  constructor() { for (let i = 0; i < 5 ; i++) {
+    this.cardapio.push(
+      {
+        nome: 'Whooper',
+        preco: 25.50,
+        qtd: 0
+      }
+    );
+  } }
 
   ngOnInit() {
   }
+  add(i) {
+    this.cardapio[i].qtd++;
+  }
+  minus(i) {
+    if (this.cardapio[i].qtd > 0) {
 
+      this.cardapio[i].qtd--;
+    }    else {
+      this.cardapio[i].qtd = 0;
+    }
+  }
 }
